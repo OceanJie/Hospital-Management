d4ce0ae9df6cfe2e3c339688c792ef9a95080b14
blin <blin@iastate.edu>
2019-10-29 17:26:34 -0500

    doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,7 +1,19 @@
 
 public class Doctor {
-	
-	String doctor_name;
-	
-    
+
+	private String doctor_name;
+
+	public Doctor(string name)
+	{
+		doctor_name = name;
+	}
+	public void setName(string name)
+	{
+		doctor_name = name;
+	}
+	public string getName()
+	{
+		return doctor_name;
+	}
+
 }
