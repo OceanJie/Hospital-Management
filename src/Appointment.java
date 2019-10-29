33fe4e726c74cc31f263a75d79a068b9bf2a88d2
Nicholas <nicholas@mac-air-039.cs.iastate.edu>
2019-10-29 17:22:52 -0500

    a

--- src/Appointment.java
+++ src/Appointment.java
@@ -40,5 +40,4 @@ public class Appointment {
   {
     doctor_name = name;
   }
-
 }
--- src/Patient.java
+++ src/Patient.java
@@ -2,36 +2,54 @@ package iteration.one;
 
 /**
  * Patient class
- * 
+ *
  * @author Nicholas Ong
  */
 public class Patient {
-	
+
 	private String name;
-	
+	private Appointment ap;
+
 	public Patient(String name) {
 		this.name = name;
 	}
-	
+
+	/**
+	 * Get this patient's name
+	 *
+	 * @return The name of this patient
+	 */
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
 	 * Adding this patient's appointment time to the list
-	 * 
+	 *
+	 * Pre-condition: The time is available
+	 *
 	 * @param day - Name of the days (Ex: Monday)
-	 * @param hours - The 
-	 * @param minutes
-	 * @param isAM
-	 * @return
+	 * @param hours - The hour of the appointment time in 24-hr format
+	 * @param minutes - The minute of the appointment time
+	 * @return True if making an appointment is successful, false otherwise
 	 */
-	public boolean makeAppointment(String day, int hours, int minutes, boolean isAM) {
-		
-		return false;
+	public boolean makeAppointment(String day, int hours, int minutes) {
+		if(day == NULL || hours < 0 || hours > 24 || minutes > 60 || minutes < 0)
+			return false;
+
+		Random rand = new Random();
+		int id = hours * minutes + rand.nextInt(hours + minutes);
+		ap = new Appointment(id);
+		ap.setpatient_name(getName());
+		return true;
 	}
-	
+
+	/**
+	 * Adding this patient's appointment time to the list
+	 *
+	 * @return The prescription for this patient
+	 */
 	public String getPrescription() {
-		return null;
+		return ap.getprescription();
 	}
 }
