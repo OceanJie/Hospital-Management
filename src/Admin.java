db4db72bbfc687f0a396206716157af72577af4c
Sheng Jie Ooi <sheng10100@gmail.com>
2019-10-29 18:36:08 -0500

    updated admin class

--- src/Admin.java
+++ src/Admin.java
@@ -3,6 +3,10 @@ package iteration.one;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Admin class
+ * @author Sheng Jie Ooi
+ */
 public class Admin {
 	
 	private List<Patient> listofPatient;
@@ -12,20 +16,41 @@ public class Admin {
 		listofPatient = new ArrayList<>();
 		listofDoctor = new ArrayList<>();
 	}
-	
+
 	/**
-	 * 
-	 * @return
+	 * return true when successfully added a new doctor obj into the ArrayList
+	 * return false when doctor already exist
+	 * @param doctor_name
+	 * @return boolean
 	 */
-	public boolean addDoctor() {
-		return false;
+	public boolean addDoctor(String doctor_name) {
+		Doctor doc = new Doctor (doctor_name);
+		if(listofDoctor.indexof(doc)== -1) {
+			System.out.println(doctor_name+" already exist")
+			return false;
+		}
+		else{
+			listofDoctor.add(doc);
+		}
+		return true;
 	}
-	
+
 	/**
-	 * 
-	 * @return
+	 * return true when successfully added a new patient obj into the ArrayList
+	 * return false when patient already exist
+	 * @param patient_name
+	 * @return boolean
 	 */
-	public boolean addPatient() {
-		return false;
+	public boolean addPatient(String patient_name) {
+		Patient p = new Patient (patient_name);
+		
+		if(listofPatient.indexof(p)== -1) {
+			System.out.println(patient_name+" already exist")
+			return false;
+		}
+		else{
+			listofPatient.add(p);
+		}
+		return true;
 	}
 }
