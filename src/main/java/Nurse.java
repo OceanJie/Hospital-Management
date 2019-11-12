e034db85fb1222ec8088f31022756152d26faa1f
Nicholas <nicholas@mac-air-039.cs.iastate.edu>
2019-11-12 16:50:48 -0600

    small changes

--- src/main/java/Nurse.java
+++ src/main/java/Nurse.java
@@ -1,14 +1,47 @@
 package main.java;
 
+/**
+ * Nurse class
+ * 
+ * @author Nicholas Ong
+ */
 public class Nurse {
 	
 	private String name;
+	private Salary salary;
 	
-	public Nurse(String name) {
+	public Nurse(String name, double salary) {
+		this(name, salary, 0);
+	}
+	
+	public Nurse(String name, double salary, double bonus) {
 		this.name = name;
+		this.salary = new Salary(salary, bonus);
 	}
 	
 	public String getName() {
 		return name;
+	}
+	
+	public Salary getSalary() {
+		return salary;
+	}
+	
+	public boolean setSalary(double newSalary) {
+		if(newSalary <= 0) 
+			return false;
+		else {
+			salary.setSalary(newSalary);
+			return true;	
+		}
+	}
+	
+	public boolean setBonus(double newBonus) {
+		if(newBonus <= 0) 
+			return false;
+		else {
+			salary.setBonus(newBonus);
+			return true;
+		}
 	}
 }
