bc67e8c677bce2fe5e98ab061c9cd27f7cf789d7
BlazeFatality <nicholasongwk98@gmail.com>
2019-11-18 22:22:13 -0600

    small changes

--- src/main/java/Insurance.java
+++ src/main/java/Insurance.java
@@ -2,4 +2,24 @@ package main.java;
 
 public class Insurance {
 
+	private Patient patient;
+	private String[] insuranceNames = {"SafetyFirst", "Veteran", "Senior Citizen"};
+	
+	public Insurance(Patient patient) {
+		this.patient = patient;
+	}
+	
+	public Patient getPatient() {
+		return patient;
+	}
+	
+	public boolean getInsurance(String insuranceName, double amount) {
+		
+		for(int i=0;i<insuranceNames.length;i++) 
+			if(insuranceName.equals(insuranceNames[i]))
+				if(amount > 0)
+					return true;
+		
+		return false;
+	}
 }
